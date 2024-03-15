fd9dcab6c33775ed0490443b87adbb99ced44494
Chamith Jayaweera <chamithlkml@gmail.com>
2024-03-15 00:43:19 +0000

    Used Pagination component

---
+++ resources/js/Components/UI/Pagination.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav aria-label="Page navigation example" class="mt-4 text-center">
+    <ul class="inline-flex -space-x-px text-base h-10">
+    <li>
+        <Link :href="links.prev_page_url" class="paginate-previous">Previous</Link>
+    </li>
+    <li>
+        <Link :href="links.first_page_url" class="paginate-button">First Page</Link>
+    </li>
+    <li>
+        <Link aria-current="page" class="paginate-button-active">{{ links.current_page }}</Link>
+    </li>
+    <li>
+        <Link :href="links.last_page_url" class="paginate-button">Last Page</Link>
+    </li>
+    <li>
+        <Link :href="links.next_page_url" class="paginate-next">Next</Link>
+    </li>
+    </ul>
+</nav>
+</template>
+<script setup>
+import { Link } from '@inertiajs/vue3'
+defineProps({
+  links: Object
+})
+</script>
--- resources/js/Pages/Listing/IndexPage.vue
+++ resources/js/Pages/Listing/IndexPage.vue
@@ -1,37 +1,18 @@
 <template>
     <div>
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
-            <Listing :user="user" v-for="listing in listings" :key="listing.id" :listing="listing"/>
+            <Listing :user="user" v-for="listing in paginatedListings.data" :key="listing.id" :listing="listing"/>
         </div>
-        <nav aria-label="Page navigation example" class="mt-4 text-center">
-            <ul class="inline-flex -space-x-px text-base h-10">
-            <li>
-                <Link :href="props.paginatedListings.prev_page_url" class="paginate-previous">Previous</Link>
-            </li>
-            <li>
-                <Link :href="props.paginatedListings.first_page_url" class="paginate-button">First Page</Link>
-            </li>
-            <li>
-                <Link aria-current="page" class="paginate-button-active">{{ props.paginatedListings.current_page }}</Link>
-            </li>
-            <li>
-                <Link :href="props.paginatedListings.last_page_url" class="paginate-button">Last Page</Link>
-            </li>
-            <li>
-                <Link :href="props.paginatedListings.next_page_url" class="paginate-next">Next</Link>
-            </li>
-            </ul>
-        </nav>
+        <Pagination :links="paginatedListings"/>
     </div>
 </template>
 <script setup>
-import { usePage, Link } from '@inertiajs/vue3'
+import { usePage } from '@inertiajs/vue3'
 import { computed } from 'vue'
 import Listing from '@/Pages/Listing/Index/Components/Listing.vue'
+import Pagination from '@/Components/UI/Pagination.vue'
 const user = computed(() => usePage().props.user)
-const props = defineProps({
+defineProps({
   paginatedListings: Object
 })
-const listings = props.paginatedListings.data
-console.log(props.paginatedListings.next_page_url)
 </script>
